b677628992b3109dc24fa0c88ff94fbe9527c205
steven <pospinachico@gmail.com>
2026-05-07 20:10:53 -0500

    Actualización
    Hasta la diapo 17

--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+import { AnimalService } from '../../services/animal-service';
+import { take } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef } from '@angular/core';
-import { AnimalService } from '../../services/animal-service';
+
 
 @Component({
   selector: 'app-animal-component',
-  imports: [CommonModule],
+  imports: [CommonModule],
   templateUrl: './animal-component.html',
   styleUrl: './animal-component.css',
 })
 export class AnimalComponent {
   animalList: any = [];
+  animalForm: FormGroup | any;
 
-  constructor(private cd: ChangeDetectorRef, private animalService: AnimalService) { }
+  constructor(private cd: ChangeDetectorRef, private animalService: AnimalService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   getAllAnimals() {
     this.animalService.getAllAnimalsData().subscribe((data: {}) => {
@@ -21,10 +30,33 @@ export class AnimalComponent {
     });
   }
   ngOnInit() {
+    this.animalForm = this.formBuilder.group({
+      nombre: '',
+      edad: 0,
+      tipo: ''
+    });
+  }
+  ngOnChanges() {
     this.getAllAnimals();
   }
-   ngOnChanges() {
-    this.getAllAnimals();
+  newMessage(messageText: string) {
+    this.toastr.success('Clic aquí para actualizar la lista', messageText)
+      .onTap
+      .pipe(take(1))
+      .subscribe(() => window.location.reload());
   }
+  newAnimalEntry() {
+    this.animalService.newAnimal(this.animalForm.value).subscribe(
+      () => {
+        //Redirigiendo a la ruta actual /inicio y recargando la ventana
+        this.router.navigate(['/inicio'])
+          .then(() => {
+            this.newMessage('Registro exitoso');
+          })
+      }
+    );
+  }
+
+
 }
 
