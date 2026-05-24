4447bc035cee93f4fb6bb1827b4931dedcc36982
ParraDav <df.parra.07@gmail.com>
2026-05-24 12:50:03 -0500

    .

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,10 +1,88 @@
 import { Component } from '@angular/core';
+
 import { Navbar } from '../navbar/navbar';
+
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule
+} from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+
+import { AuthService } from '../../services/auth-service';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [Navbar],
+  imports: [
+    Navbar,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+export class Login {
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+
+    this.loginForm = this.fb.group({
+
+      email: [''],
+      password: ['']
+
+    });
+
+  }
+
+  login() {
+
+    this.authService
+      .login(this.loginForm.value)
+      .subscribe({
+
+        next: (res: any) => {
+
+          console.log(res);
+
+          // Guardar token
+          localStorage.setItem(
+            'token',
+            res.token
+          );
+
+          // Guardar rol
+          localStorage.setItem(
+            'rol',
+            res.rol
+          );
+
+          alert('Login exitoso');
+
+          this.router.navigate(['/cursos']);
+
+        },
+
+        error: (err) => {
+
+          console.log(err);
+
+          alert('Credenciales incorrectas');
+
+        }
+
+      });
+  }
+}
--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
+
 import { Navbar } from '../navbar/navbar';
+
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule
+} from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+
+import { AuthService } from '../../services/auth-service';
+
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [Navbar],
+  imports: [
+    Navbar,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register { }
+
+export class Register {
+
+  registerForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+
+    this.registerForm = this.fb.group({
+
+      nombre: [''],
+      email: [''],
+      password: [''],
+      rol: ['estudiante']
+
+    });
+
+  }
+
+  register() {
+
+    this.authService
+      .register(this.registerForm.value)
+      .subscribe({
+
+        next: (res: any) => {
+
+          console.log(res);
+
+          alert('Usuario registrado');
+
+          this.router.navigate(['/login']);
+
+        },
+
+        error: (err) => {
+
+          console.log(err);
+
+          alert('Error al registrar');
+
+        }
+
+      });
+  }
+}
