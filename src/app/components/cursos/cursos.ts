327f78d83bc4fbf49ff61cbd437424616c6da450
Steven <Pospinachico@gmail.com>
2026-05-24 22:42:03 -0500

    A

--- src/app/components/cursos/cursos.ts
+++ src/app/components/cursos/cursos.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { Navbar } from '../navbar/navbar';
 import { CursoService } from '../../services/curso-service';
 
 @Component({
@@ -10,7 +9,6 @@ import { CursoService } from '../../services/curso-service';
   imports: [
     CommonModule,
     RouterModule,
-    Navbar
   ],
   templateUrl: './cursos.html',
   styleUrl: './cursos.css'
