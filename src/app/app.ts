619a81e9ba6ca7c4de28b039621c6625d2304c77
Steven <Pospinachico@gmail.com>
2026-05-24 22:30:39 -0500

    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { Navbar } from './components/navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/cursos/cursos.ts
+++ src/app/components/cursos/cursos.ts
@@ -2,13 +2,56 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
+import { CursoService } from '../../services/curso-service';
+
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [CommonModule, RouterModule, Navbar],
+  imports: [
+    CommonModule,
+    RouterModule,
+    Navbar
+  ],
   templateUrl: './cursos.html',
   styleUrl: './cursos.css'
 })
+
 export class Cursos {
+
+  cursos: any[] = [];
+
+  constructor(
+    private cursoService: CursoService
+  ) { }
+
+  ngOnInit() {
+
+    this.obtenerCursos();
+
+  }
+
+  obtenerCursos() {
+
+    this.cursoService
+      .getCursos()
+      .subscribe({
+
+        next: (res: any) => {
+
+          console.log(res);
+
+          this.cursos = res;
+
+        },
+
+        error: (err) => {
+
+          console.log(err);
+
+        }
+
+      });
+
+  }
 
 }
