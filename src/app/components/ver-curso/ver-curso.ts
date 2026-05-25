32d8b135198eda5a1cc285a2efb80b7e2606a633
Steven <Pospinachico@gmail.com>
2026-05-24 22:42:28 -0500

    AA

--- src/app/components/ver-curso/ver-curso.ts
+++ src/app/components/ver-curso/ver-curso.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-ver-curso',
   standalone: true,
-  imports: [Navbar],
   templateUrl: './ver-curso.html',
   styleUrl: './ver-curso.css'
 })
